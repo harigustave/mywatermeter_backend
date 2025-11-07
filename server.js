0e10de85a8d76292fed23c5b6b174eef5dcf615b
harigustave <harigustave@gmail.com>
2025-11-07 09:58:13 +0200

    Latest commit 07.11.2025

--- server.js
+++ server.js
@@ -74,4 +74,5 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
